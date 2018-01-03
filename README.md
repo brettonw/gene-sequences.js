cd1acb2632372c782b1be68645a1f1e962c3c0c2
Bretton Wade <Bretton.Wade@gmail.com>
2018-01-03 17:40:06 +0000

    analyze button, defaults, and direct deployment from github pages

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-# gene-sequences.js
+# [gene-sequences.js](https://brettonw.github.io/gene-sequences.js/index.html)
+
 Analysis and visualization of HIV gene sequences
 
-# Data Sets
-Representing three different individuals from a very famous study on HIV sequence evolution:
 
-The original paper by Shankarappa can be found at https://www.ncbi.nlm.nih.gov/pmc/articles/PMC113104/
+# Data Set
+A JSON-format data set that represents the C2-V5 region of the *env* gene from three different individuals, taken from a very famous study on HIV sequence evolution by Shankarappa (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC113104/).
 
-It has been cited by close to 1000 other research papers on viral evolution particularly HIV.
-
-Each text file is sequence data from a different individual, where there are multiple sequences per subject time point. The naming system is the patient number, then c or p (cells/plasma), then the month of sampling, then the clone. So P7c80 1075 means patient 7 sequence from cells 80 months post seroconversion clone #1075.
+There are multiple sequences per subject time point. The naming system is the patient number, then c or p (cells/plasma), then the month of sampling, then the clone. So P7c80 1075 means patient 7 sequence from cells 80 months post seroconversion clone #1075.
 
 There are multiple sequences per subject time point.  The quasispecies (cloud of related viruses) inside a person gets more diverse with time.  Though very late in infection the most fit virus is thought to outgrow the rest of the viruses so you may get a decrease in viral diversity.
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let Http = Bedrock.Http;
+let analyze;
 let main = function () {
     let now = new Date ().getTime ();
     Http.get ("subjects.json?" + now, function (sequences) {
@@ -13,7 +14,6 @@ let main = function () {
         for (let sequence of sequences) {
             let sequenceStr = sequence["Sequence"];
             sequence["Length"] = sequenceStr.length;
-            sequence["Sequence"] = (sequenceStr + "-------------------------").substring(0, 540);
         }
 
         let CF = Bedrock.CompareFunctions;
@@ -26,6 +26,7 @@ let main = function () {
         // build the database filter
         Bedrock.Database.Container.new ({
             database: sequences,
+            filterValues: [{ field: "Patient", value: 7 }],
             onUpdate: function (db) {
                 // make a duplicate of the data before we do our analysis
                 let sequences = [];
@@ -40,118 +41,120 @@ let main = function () {
                         { name: "Patient", width: 1/16 },
                         { name: "Month", width: 1/16 },
                         { name: "Clone", width: 1/16 },
+                        { name: "Sample", width: 1/16 },
                         { name: "Length", width: 1/16 },
-                        { name: "Sequence", width: 11/16, type: Bedrock.PagedDisplay.EntryType.LEFT_JUSTIFY }
+                        { name: "Sequence", width: 21/32, type: Bedrock.PagedDisplay.EntryType.LEFT_JUSTIFY }
                     ]
                 }).makeTableWithHeader ();
 
                 // now run some analysis - what positions in the sequences vary - start by pivoting
                 // the data into strings on the positions
-                let sequenceCount = sequences.length;
-                let positions = [];
-                let sequenceStrLength = 0;
-                for (let sequence of sequences) {
-                    if (sequence["Sequence"].length !== sequenceStrLength) {
-                        sequenceStrLength = sequence["Sequence"].length;
-                        console.log ("Sequence Length: " + sequenceStrLength);
+                analyze = function () {
+                    let sequenceCount = sequences.length;
+                    let positions = [];
+                    let sequenceStrLength = 0;
+                    for (let sequence of sequences) {
+                        if (sequence["Sequence"].length !== sequenceStrLength) {
+                            sequenceStrLength = sequence["Sequence"].length;
+                            console.log ("Sequence Length: " + sequenceStrLength);
+                        }
                     }
-                }
-                for (let i = 0; i < sequenceStrLength; ++i) {
-                    positions.push ([]);
-                }
-                for (let sequence of sequences) {
-                    let sequenceStr = sequence["Sequence"];
                     for (let i = 0; i < sequenceStrLength; ++i) {
-                        positions[i].push (sequenceStr.charAt (i));
+                        positions.push ([]);
                     }
-                }
-
-                // take a look at the transitions that occur
-                let range = Object.create (null);
-                let transitions = Object.create (null);
-                for (let i = 0; i < sequenceStrLength; ++i) {
-                    let varies = false;
-                    let lastChar = positions[i][0];
-                    range[lastChar] = (range[lastChar] !== undefined) ? (range[lastChar] + 1) : 1;
-                    for (let j = 1, end = positions[i].length; j < end; ++j) {
-                        let currentChar = positions[i][j];
-                        range[currentChar] = (range[currentChar] !== undefined) ? (range[currentChar] + 1) : 1;
-                        if (currentChar !== lastChar) {
-                            //console.log("Position " + i + " transitons from '" + lastChar + "' to '" + currentChar + "'" );
-                            let transition = lastChar + currentChar;
-                            transitions[transition] = (transitions[transition] !== undefined) ? (transitions[transition] + 1) : 1;
-                            varies = true;
-                            lastChar = currentChar;
+                    for (let sequence of sequences) {
+                        let sequenceStr = sequence["Sequence"];
+                        for (let i = 0; i < sequenceStrLength; ++i) {
+                            positions[i].push (sequenceStr.charAt (i));
                         }
                     }
-                    positions[i] = varies;
-                }
 
-                // function to shrink the gene strings to only the varying components
-                let shrinkSequence= function (sequenceStr) {
-                    let shrunkSequence = "";
-                    for (let i = 0, end = sequenceStr.length; i < end; ++i) {
-                        if (positions[i] === true) {
-                            shrunkSequence += sequenceStr.charAt (i);
+                    // take a look at the transitions that occur
+                    let range = Object.create (null);
+                    let transitions = Object.create (null);
+                    for (let i = 0; i < sequenceStrLength; ++i) {
+                        let varies = false;
+                        let lastChar = positions[i][0];
+                        range[lastChar] = (range[lastChar] !== undefined) ? (range[lastChar] + 1) : 1;
+                        for (let j = 1, end = positions[i].length; j < end; ++j) {
+                            let currentChar = positions[i][j];
+                            range[currentChar] = (range[currentChar] !== undefined) ? (range[currentChar] + 1) : 1;
+                            if (currentChar !== lastChar) {
+                                //console.log("Position " + i + " transitons from '" + lastChar + "' to '" + currentChar + "'" );
+                                let transition = lastChar + currentChar;
+                                transitions[transition] = (transitions[transition] !== undefined) ? (transitions[transition] + 1) : 1;
+                                varies = true;
+                                lastChar = currentChar;
+                            }
+                        }
+                        positions[i] = varies;
+                    }
+
+                    // function to shrink the gene strings to only the varying components
+                    let shrinkSequence = function (sequenceStr) {
+                        let shrunkSequence = "";
+                        for (let i = 0, end = sequenceStr.length; i < end; ++i) {
+                            if (positions[i] === true) {
+                                shrunkSequence += sequenceStr.charAt (i);
+                            }
+                        }
+                        //console.log ("Shrunk sequence from " + sequenceStr.length + " to " + shrunkSequence.length);
+                        return shrunkSequence;
+                    };
+
+                    // and now shrink all the sequences
+                    for (let sequence of sequences) {
+                        let sequenceStr = sequence["Sequence"];
+                        sequence["Sequence"] = shrinkSequence (sequence["Sequence"]);
+                    }
+
+                    let shrunkSequenceStrLength = sequences[0]["Sequence"].length;
+                    console.log ("Shrunk Sequence Length to: " + shrunkSequenceStrLength);
+
+                    // spew a few stats on the range and transitions
+                    let rangeKeys = Object.keys (range);
+                    let totalRangeCount = 0;
+                    for (let rangeKey of rangeKeys) {
+                        totalRangeCount += range[rangeKey];
+                    }
+                    console.log ("Total Range Count: " + totalRangeCount);
+                    for (let rangeKey of rangeKeys) {
+                        let percentage = range[rangeKey] / totalRangeCount;
+                        console.log ("Range Value: " + rangeKey + " -> " + range[rangeKey] + " (" + Math.floor (100 * percentage) + "%)");
+                        //range[rangeKey] = percentage;
+                    }
+
+                    let totalTransitionCount = 0;
+                    let transitionKeys = Object.keys (transitions);
+                    for (let transition of transitionKeys) {
+                        totalTransitionCount += transitions[transition];
+                    }
+                    console.log ("Total Transition Count: " + totalTransitionCount);
+                    for (let transition of transitionKeys) {
+                        let percentage = transitions[transition] / totalTransitionCount;
+                        console.log ("Transition: " + transition + " -> " + transitions[transition] + " (" + Math.floor (100 * percentage) + "%)");
+                        //transitions[transition] = percentage;
+                    }
+
+                    // confirm that transitions are bidirectional
+                    let allPass = true;
+                    for (let a = 0, end = rangeKeys.length - 1; a < end; ++a) {
+                        let rangeKeyA = rangeKeys[a];
+                        for (let b = a + 1; b < rangeKeys.length; ++b) {
+                            let rangeKeyB = rangeKeys[b];
+                            let transitionAB = rangeKeyA + rangeKeyB;
+                            let transitionBA = rangeKeyB + rangeKeyA;
+                            let pass = withinTolerance (transitions[transitionAB], transitions[transitionBA], 0.05);
+                            allPass &= pass;
+                            console.log (transitionAB + " " + (pass ? "==" : "!=") + " " + transitionBA + " (" + (pass ? "PASS" : "FAIL") + ")");
                         }
                     }
-                    //console.log ("Shrunk sequence from " + sequenceStr.length + " to " + shrunkSequence.length);
-                    return shrunkSequence;
-                };
-
-                // and now shrink all the sequences
-                for (let sequence of sequences) {
-                    let sequenceStr = sequence["Sequence"];
-                    sequence["Sequence"] = shrinkSequence(sequence["Sequence"]);
-                }
-
-                let shrunkSequenceStrLength = sequences[0]["Sequence"].length;
-                console.log ("Shrunk Sequence Length to: " + shrunkSequenceStrLength);
-
-                // spew a few stats on the range and transitions
-                let rangeKeys = Object.keys(range);
-                let totalRangeCount = 0;
-                for (let rangeKey of rangeKeys) {
-                    totalRangeCount += range[rangeKey];
-                }
-                console.log ("Total Range Count: " + totalRangeCount);
-                for (let rangeKey of rangeKeys) {
-                    let percentage = range[rangeKey] / totalRangeCount;
-                    console.log("Range Value: " + rangeKey + " -> " + range[rangeKey] + " (" + Math.floor (100 * percentage) + "%)");
-                    //range[rangeKey] = percentage;
-                }
-
-                let totalTransitionCount = 0;
-                let transitionKeys = Object.keys (transitions);
-                for (let transition of transitionKeys) {
-                    totalTransitionCount += transitions[transition];
-                }
-                console.log ("Total Transition Count: " + totalTransitionCount);
-                for (let transition of transitionKeys) {
-                    let percentage = transitions[transition] / totalTransitionCount;
-                    console.log ("Transition: " + transition + " -> " + transitions[transition] + " (" + Math.floor (100 * percentage) + "%)");
-                    //transitions[transition] = percentage;
-                }
-
-                // confirm that transitions are bidirectional
-                let allPass = true;
-                for (let a = 0, end = rangeKeys.length - 1; a < end; ++a) {
-                    let rangeKeyA = rangeKeys[a];
-                    for (let b = a + 1; b < rangeKeys.length; ++b) {
-                        let rangeKeyB = rangeKeys[b];
-                        let transitionAB = rangeKeyA + rangeKeyB;
-                        let transitionBA = rangeKeyB + rangeKeyA;
-                        let pass = withinTolerance(transitions[transitionAB], transitions[transitionBA], 0.05);
-                        allPass &= pass;
-                        console.log (transitionAB + " " + (pass ? "==" : "!=") + " " + transitionBA + " (" + (pass ? "PASS" : "FAIL") + ")");
+                    if (!allPass) {
+                        console.log ("ALERT: TRANSITIONS ARE NOT BI-DIRECTIONAL");
                     }
-                }
-                if (!allPass) {
-                    console.log ("ALERT: TRANSITIONS ARE NOT BI-DIRECTIONAL");
-                }
 
-                // convert the transition percentages to lengths
-
+                    // convert the transition percentages to lengths
+                }
             }
         });
     });
